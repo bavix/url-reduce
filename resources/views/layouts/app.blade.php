e3c05a00bcb72a96f176cf54539edbba3802f3c6
REZ1DENT3 <maksim.babichev95@gmail.com>
2017-04-24 16:32:48 +0300

    landing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,16 +2,22 @@
 <html lang="{{ config('app.locale') }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    @if(isset($title))
+        <title>{{ $title }} - {{ config('app.name', 'Laravel') }}</title>
+    @else
+        <title>{{ config('app.name', 'Laravel') }}</title>
+    @endif
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{--<link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
+    <link href="{{ asset('node_modules/bootstrap/dist/css/bootstrap.min.css')  }}" rel="stylesheet"/>
+    <link href="{{ asset('css/sot.css')  }}" rel="stylesheet"/>
 
     <!-- Scripts -->
     <script>
@@ -21,67 +27,204 @@
     </script>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+            data-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false"
+            aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">СЮТ</a>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
+    <div class="collapse navbar-collapse" id="navbarDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ url('/') }}">Новости <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item drop">
+                <a class="nav-link" href="{{ url('/albums') }}">Альбомы</a>
+            </li>
+            <li class="nav-item drop">
+                <a class="nav-link" href="{{ url('/polls') }}">Опросы</a>
+            </li>
+            <li class="nav-item drop">
+                <a class="nav-link" href="{{ url('/poll/1') }}">Подача заявления</a>
+            </li>
+            <li class="nav-item drop">
+                <a class="nav-link" href="{{ url('/poll/2') }}">+</a>
+            </li>
+            <li class="nav-item drop">
+                <a class="nav-link" href="{{ url('/poll/3') }}">-</a>
+            </li>
+            {{--<li class="nav-item dropdown">--}}
+            {{--<a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown"--}}
+            {{--aria-haspopup="true" aria-expanded="false">Страницы</a>--}}
+            {{--<div class="dropdown-menu" aria-labelledby="dropdown01">--}}
+            {{--<a class="dropdown-item" href="{{ url('/page/1-info') }}">Страница 1</a>--}}
+            {{--<a class="dropdown-item" href="{{ url('/page/2-info') }}">Страница 2</a>--}}
+            {{--<a class="dropdown-item" href="{{ url('/page/3-info') }}">Страница 3</a>--}}
+            {{--<a class="dropdown-item" href="{{ url('/page/4-info') }}">Страница 4</a>--}}
+            {{--</div>--}}
+            {{--</li>--}}
+        </ul>
+    </div>
+</nav>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
+<div class="container">
+    <div class="row">
+        <div class="col-md-9">
+            @yield('content')
+        </div>
+        <div id="blocks" class="col-md-3">
+            <div class="card">
+                <div class="card-block">
+                    <h5 class="card-title">Важная информация</h5>
+                    <ul id="menu" class="nav bd-sidenav">
+                        <li>
+                            <a href="{{ url('/page/1') }}">Страница 1</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/page/2') }}">Страница 2</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/page/3') }}">Страница 3</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/page/4') }}">Страница 4</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/page/5') }}">Страница 5</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/page/6') }}">Страница 6</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/page/7') }}">Страница 7</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/page/8') }}">Страница 8</a>
+                        </li>
                     </ul>
                 </div>
             </div>
-        </nav>
-
-        @yield('content')
+            <div class="clearfix"></div>
+            <div class="card">
+                <div class="card-block">
+                    <h5 class="card-title">Last news</h5>
+                    <p class="card-text">Java Script</p>
+                    <a href="#" class="btn btn-primary btn-sm btn-block">Self</a>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="card">
+                <div class="card-block">
+                    <h5 class="card-title">Last news</h5>
+                    <p class="card-text">Java Script</p>
+                    <a href="#" class="btn btn-primary btn-sm btn-block">Self</a>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="card">
+                <div class="card-block">
+                    <h5 class="card-title">Last news</h5>
+                    <p class="card-text">Java Script</p>
+                    <a href="#" class="btn btn-primary btn-sm btn-block">Self</a>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
     </div>
+</div>
+
+<footer class="mainfooter" role="contentinfo">
+
+    <div class="footer-middle">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <!--Column1-->
+                    <div class="footer-pad">
+                        <h4>Контакты</h4>
+                        <address>
+                            <ul class="list-unstyled">
+                                <li>
+                                    г. БЕЛОРЕЧЕНСК<br>
+                                    ул. 8 МАРТА, д. 57<br>
+                                    КРАСНОДАРСКИЙ край<br>
+                                    352630<br><br/>
+                                </li>
+                                <li>
+                                    Телефоны: 86155-33803<br/>
+                                    Электронная почта: nat_krakovsk@mail.ru<br/>
+                                </li>
+                            </ul>
+                        </address>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <!--Column1-->
+                    <div class="footer-pad">
+                        <h4>Popular Services</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="#"></a></li>
+                            <li><a href="#">Payment Center</a></li>
+                            <li><a href="#">Contact Directory</a></li>
+                            <li><a href="#">Forms</a></li>
+                            <li><a href="#">News and Updates</a></li>
+                            <li><a href="#">FAQs</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <!--Column1-->
+                    <div class="footer-pad">
+                        <h4>Website Information</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="#">Website Tutorial</a></li>
+                            <li><a href="#">Accessibility</a></li>
+                            <li><a href="#">Disclaimer</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">FAQs</a></li>
+                            <li><a href="#">Webmaster</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <!--Column1-->
+                    <div class="footer-pad">
+                        <h4>Popular Departments</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="#">Parks and Recreation</a></li>
+                            <li><a href="#">Public Works</a></li>
+                            <li><a href="#">Police Department</a></li>
+                            <li><a href="#">Fire</a></li>
+                            <li><a href="#">Mayor and City Council</a></li>
+                            <li>
+                                <a href="#"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <!--Footer Bottom-->
+                    <p class="text-xs-center">
+                        <a class="btn btn-link" title="Babichev Maxim" href="https://babichev.net/" target="_blank">Создание сайта - Babichev</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+<script defer async src="{{ asset('node_modules/jquery/dist/jquery.min.js') }}"></script>
+<script defer async src="{{ asset('node_modules/tether/dist/js/tether.min.js') }}"></script>
+<script defer async src="{{ asset('node_modules/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 </body>
 </html>
