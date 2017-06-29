42f8be5af2bab499d1ed0548756d95daf791d187
rez1dent3 <maksim.babichev95@gmail.com>
2017-06-29 15:24:47 +0300

    upd

--- app/Http/Controllers/DebugController.php
+++ app/Http/Controllers/DebugController.php
@@ -9,6 +9,10 @@ class DebugController extends Controller
 
     public function index(Request $request)
     {
+        
+        var_dump( $request->getScheme() );
+        var_dump( $request->secure() );
+
         dump( $request );
         die;
     }
