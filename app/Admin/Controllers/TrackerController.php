e261cabdb72780dda96b2c572b1c2307705cb54c
rez1dent3 <maksim.babichev95@gmail.com>
2017-07-06 11:04:22 +0300

    upd tracker

--- app/Admin/Controllers/TrackerController.php
+++ app/Admin/Controllers/TrackerController.php
@@ -6,11 +6,13 @@ use App\Admin\Extensions\LG\Trash;
 use App\Http\Controllers\Controller;
 use App\Models\AlbumModel;
 use App\Models\TrackerModel;
+use Bavix\Helpers\JSON;
 use Encore\Admin\Controllers\ModelForm;
 use App\Facades\Admin;
 use App\Accessor\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
+use Laravelrus\LocalizedCarbon\LocalizedCarbon;
 
 class TrackerController extends AdminController
 {
@@ -30,12 +32,33 @@ class TrackerController extends AdminController
 
             $grid->id('ID')->sortable();
 
-            $grid->column('ip', 'IP адрес')->sortable();
-            $grid->column('referer', 'Откуда');
-            $grid->column('url', 'Куда');
-            $grid->column('language', 'Язык');
+            $grid->column('ip', 'IP адрес')
+                ->display(function ($ip)
+                {
+                    return '<span class="badge bg-green">' . $ip . '</span>';
+                })
+                ->sortable();
 
-            $grid->column('created_at', 'Время визита');
+            $grid->column('url', 'Куда')
+                ->display(function ($url)
+                {
+                    return '<span class="badge bg-blue">' . $url . '</span>';
+                });
+
+            $grid->column('parameters', 'Параметры')
+                ->display(function ($json)
+                {
+                    return JSON::decode($json);
+                });
+
+            $grid->column('created_at', 'Время визита')
+                ->display(function ($dateTime)
+                {
+                    $carbon = LocalizedCarbon::createFromFormat('Y-m-d H:i:s', $dateTime);
+                    $local  = LocalizedCarbon::instance($carbon);
+
+                    return '<span class="badge bg-yellow">' . $local->diffForHumans() . '</span>';
+                });
 
             $grid->exporter(new \App\Accessor\CsvExporter());
 
@@ -56,9 +79,8 @@ class TrackerController extends AdminController
             $form->display('id', 'ID');
 
             $form->ip('ip', 'IP');
-            $form->text('referer', 'Откуда');
             $form->text('url', 'Куда');
-            $form->text('language', 'Язык');
+            $form->text('parameters', 'Параметры');
 
             $form->ignore(['created_at', 'updated_at']);
 
