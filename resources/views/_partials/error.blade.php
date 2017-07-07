d63c0f0e51365995050e891a68747583eee39936
rez1dent3 <maksim.babichev95@gmail.com>
2017-07-07 16:13:46 +0300

    add viewport

--- resources/views/_partials/error.blade.php
+++ resources/views/_partials/error.blade.php
@@ -2,7 +2,8 @@
 <html>
 <head>
     <title>{{ $title }}</title>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
     <link href="{{ asset2('node_modules/bootstrap/dist/css/bootstrap.min.css')  }}" rel="stylesheet"/>
     @if (isset($reload))
