38e687314c7fc0f858544f2ce798b56f969f0c02
rez1dent3 <maksim.babichev95@gmail.com>
2017-07-10 15:22:26 +0300

    temp api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,42 @@ use Illuminate\Http\Request;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+
+Route::get('v1/{model}', function (Request $request, $model)
+{
+
+    $preload = $request->query('preload', []);
+
+    $terms   = $request->query('terms', []);
+    $greater = $request->query('greater', []);
+    $less    = $request->query('less', []);
+    $sort    = $request->query('sort', ['id' => 'desc']);
+
+    $model = strtolower($model);
+    $model = '\\App\\Models\\' . ucfirst($model) . 'Model';
+
+    $object = $model::with($preload);
+
+    foreach ($terms as $column => $value)
+    {
+        $object->where($column, $value);
+    }
+
+    foreach ($greater as $column => $value)
+    {
+        $object->where($column, '>', $value);
+    }
+
+    foreach ($less as $column => $value)
+    {
+        $object->where($column, '<', $value);
+    }
+
+    foreach ($sort as $column => $direction)
+    {
+        $object->orderBy($column, $direction);
+    }
+
+    return $object->paginate(50);
+
+});
